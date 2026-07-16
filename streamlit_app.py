525a39c7d0125dee8b2ea0c1bcad05e7618b8e28
Emmanuel Baah Gyan <99558022+Nanaquame1@users.noreply.github.com>
2026-07-17 00:02:28 +0400

    Integrate smoothiefroot nutrition information display
    Added a section to display nutrition information for watermelon from the smoothiefroot API.

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,12 +2,8 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 
-# Write directly to the app
 st.title(':cup_with_straw: Customize Your Smoothie! :cup_with_straw:')
-st.write(
-    """Choose the fruits you want in your custom Smoothie!
-    """
-)
+st.write("""Choose the fruits you want in your custom Smoothie!""")
 
 name_on_order = st.text_input('Name on Smoothie:')
 st.write('The name on your Smoothie will be:', name_on_order)
@@ -23,19 +19,21 @@ ingredients_list = st.multiselect(
     )
 
 if ingredients_list:
-        import requests
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
     ingredients_string = ''
-    
+
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-    
+
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
             values ('""" + ingredients_string + """','""" + name_on_order + """')"""
-    
+
     time_to_insert = st.button('Submit Order')
-    
+
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
+
+    # New section to display smoothiefroot nutrition information
+    import requests
+    smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+    sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
